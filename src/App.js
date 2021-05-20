cad46c4f5a599ea7de0a6e1665018e2f815eac89
Anz <g_anz@mail.ru>
2021-05-20 17:39:16 +0300

    add call function

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import { useRef, useState } from "react";
 
 function App() {
-  const [isStream, setIsStream] = useState(false);
+  const [buttons, setButtons] = useState({ start: false, call: false, hangup: false });
+
   const video = useRef(null);
 
   async function start() {
     console.log('Requesting local stream');
     try {
       video.current.srcObject = await navigator.mediaDevices.getUserMedia({ audio: true, video: true });
-      setIsStream(true)
+      setButtons({ ...buttons, start: true })
       console.log('Received local stream');
     } catch (e) {
       alert(`getUserMedia() error: ${e}`);
@@ -16,38 +17,38 @@ function App() {
   }
 
   async function call() {
-    callButton.disabled = true;
-    hangupButton.disabled = false;
+    setButtons({ ...buttons, call: true, hangup: false })
     console.log('Starting call');
-    
-    const videoTracks = localStream.getVideoTracks();
-    const audioTracks = localStream.getAudioTracks();
+
+    const videoTracks =  video.current.getVideoTracks();
+    const audioTracks =  video.current.getAudioTracks();
 
 
-    if (videoTracks.length > 0) {
-        console.log(`Using video device: ${videoTracks[0].label}`);
-    }
-    if (audioTracks.length > 0) {
-        console.log(`Using audio device: ${audioTracks[0].label}`);
-    }
+    // if (videoTracks.length > 0) {
+    //   console.log(`Using video device: ${videoTracks[0].label}`);
+    // }
+    // if (audioTracks.length > 0) {
+    //   console.log(`Using audio device: ${audioTracks[0].label}`);
+    // }
 
-    const configuration = {};
-    pc1 = new RTCPeerConnection(configuration);
-    pc1.addEventListener('icecandidate', e => onIceCandidate(pc1, e));
-    pc1.addEventListener('iceconnectionstatechange', e => onIceStateChange(pc1, e));
+    // const configuration = {};
+    // pc1 = new RTCPeerConnection(configuration);
+    // pc1.addEventListener('icecandidate', e => onIceCandidate(pc1, e));
+    // pc1.addEventListener('iceconnectionstatechange', e => onIceStateChange(pc1, e));
 
-    localStream.getTracks().forEach(track => pc1.addTrack(track, localStream));
-    console.log('Added local stream to pc1');
+    // localStream.getTracks().forEach(track => pc1.addTrack(track, localStream));
+    // console.log('Added local stream to pc1');
 
-    try {
-        console.log('pc1 createOffer start');
-        const offer = await pc1.createOffer(offerOptions);
-        await onCreateOfferSuccess(offer);
-    } catch (e) {
-        onCreateSessionDescriptionError(e);
-    }
-}
+    // try {
+    //   console.log('pc1 createOffer start');
+    //   const offer = await pc1.createOffer(offerOptions);
+    //   await onCreateOfferSuccess(offer);
+    // } catch (e) {
+    //   onCreateSessionDescriptionError(e);
+    // }
+  }
 
+  console.log(buttons);
   return (
     <div className="App">
       <header className="App-header">
@@ -57,7 +58,7 @@ function App() {
 
         <video id="localVideo" playsInline autoPlay muted ref={video}></video>
         <div className="box">
-          <button onClick={start} disabled={isStream}>Start</button>
+          <button onClick={start} disabled={buttons.start}>Start</button>
           <button id="callButton" onClick={call}>Call</button>
           <button id="hangupButton">Hang Up</button>
         </div>
