117195679cc156db64e68c4123f797a4dd870ea9
Anz <g_anz@mail.ru>
2021-05-20 11:52:01 +0300

    add ws server

---
+++ wsserver.js
@@ -0,0 +1,44 @@
+const WebSocket = require('ws');
+
+var clients = {};
+var offers = [];
+
+
+const ws = new WebSocket.Server({ host: "10.0.11.47", port: 8080 });
+
+ws.on('connection', function (ws) {
+
+    var id = Math.random();
+    clients[id] = ws;
+
+    ws.on('message', function (response) {
+
+        let message = JSON.parse(response);
+
+        switch (message.type) {
+            case "video-offer":
+                offers.push({ id, ...message });
+                break;
+
+            case "delete-offer":
+                offers = offers.filter(value => {
+                    return value.id !== message.id;
+                });
+            default:
+                break;
+        }
+
+        for (var key in clients) {
+            clients[key].send(JSON.stringify({ type: "video-offer", offers }));
+        }
+    });
+
+    ws.on('close', function () {
+        console.log('соединение закрыто ' + id);
+        offers = offers.filter(function (value, index, arr) {
+            return value.id !== id;
+        });
+
+    });
+
+});
